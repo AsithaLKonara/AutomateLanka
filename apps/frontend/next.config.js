f565db8a3481bd632a9abfcf7a603dcab18df12d
AsithaLKonara <asithalakmalkonara11992081@gmail.com>
2025-11-21 05:14:02 +0530

    feat: Add Sentry integration and monitoring to frontend

---
+++ apps/frontend/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Sentry configuration will be added by @sentry/nextjs plugin
+  // Run: npx @sentry/wizard@latest -i nextjs
+}
+
+module.exports = nextConfig
+
